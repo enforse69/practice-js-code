ae90f6bd3c2e90ee16470bd752aba05473791d15
enforse69 <93563241+enforse69@users.noreply.github.com>
2023-02-22 10:02:24 +1030

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*function plus(a, b, c, e ) {
+ return a + b - c - e;
+}
+
+plus(5, 10, 5, 5)
+
+alert();
+
+*/
+//-----------------------------------------
+
+/*prompt("what is your name ?", "name:")
+
+console.log();*/
+
+
+//------------------------------------------
+
+/*confirm("continue learning js?")
+
+
+console.log();*/
+
+
+//-------------------------------------------
+
+
+
+console.log(
+ "%c hey there i am logged (just something cool) on to the console",
+ "color:red;font-size:30px;font-weight:bold");
+
+ //---------------------------------------------
